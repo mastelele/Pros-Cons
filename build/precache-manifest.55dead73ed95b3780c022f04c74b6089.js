2ad35111a30f48a599f1816f2ea44e34f4d58a91
masterlele <hovnersisyan@gmail.com>
2019-12-11 13:45:49 +0400

    Added build.

---
+++ build/precache-manifest.55dead73ed95b3780c022f04c74b6089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb87b66edbec087114fd",
+    "url": "/static/css/main.82d5c89a.chunk.css"
+  },
+  {
+    "revision": "bb87b66edbec087114fd",
+    "url": "/static/js/main.5b124007.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cf5c3e4daabfff7c6812",
+    "url": "/static/js/2.10b3a8ff.chunk.js"
+  },
+  {
+    "revision": "24252aa89d34ded43596a5327d813ce2",
+    "url": "/index.html"
+  }
+];
